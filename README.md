290333009562da4e6c6feb12baa6daf6c0d1b43f
Luis Fernando Paganini <luisfernando_paganini@hotmail.com>
2021-06-01 19:36:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Projeto CRUD MVC feito com o curso da B7Web pelo aluno Luis Fernando Paganini
+
+Feito a partir do projeto mvc: https://github.com/suporteb7web/mvc
+
---
+++ mvc/src/views/partials/header.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Meu CRUD MVC</title>
+        <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css">
+    </head>
+</html>
+<body>
+    <header>
+        <h1>Header</h1>
+        <hr/>
+    </header>
+    
